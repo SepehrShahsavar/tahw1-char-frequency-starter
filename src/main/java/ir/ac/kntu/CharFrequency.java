7f6cf513c78ba7739b8384d77bd19e788d9e91fb
SepehrShahsavar <46381150+SepehrShahsavar@users.noreply.github.com>
2019-02-28 13:37:30 +0330

    Add Solution

--- src/main/java/ir/ac/kntu/CharFrequency.java
+++ src/main/java/ir/ac/kntu/CharFrequency.java
@@ -1,8 +1,26 @@
 package ir.ac.kntu;
 
-public class CharFrequency{
-    public static void main(String[] args){
-        //write your logic here
+import java.util.Scanner;
 
+public class CharFrequency {
+    public static void main(String[] args) {
+        // write your logic here
+        Scanner input = new Scanner(System.in);
+        String phrase = input.nextLine().trim();
+        String[] splited = phrase.split("\\s+");
+        String  str = String.join("", splited); 
+        String finalString = str.toLowerCase();
+        for (char i = 'a'; i <= 'z'; i++) {
+            int counter = 0;
+            for (int j = 0; j < finalString.length(); j++) {
+                if (finalString.charAt(j) == i) {
+                    counter++;
+                }
+            }
+            if (counter > 0) {
+                System.out.println(i + " : " + counter);
+            }
+        }
+        input.close();
     }
 }
